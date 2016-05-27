b56e72890cc2ce984135a6271f995cf6077c6aea
Sean Smith <sean.smith.2009@gmail.com>
2016-05-26 23:58:31 -0400

    updated Simon

--- assets/portfolio/simon_game/js/index.js
+++ assets/portfolio/simon_game/js/index.js
@@ -1,1 +1,39 @@
-var me = "hello";
+
+$('#green').click(function() {
+	green();
+});
+
+$('#red').click(function() {
+	red();
+});
+
+$('#yellow').click(function() {
+	yellow();
+});
+
+$('#blue').click(function() {
+	blue();
+});
+
+function green() {
+	$('#green').toggleClass("clickedGreen");
+}
+
+function red() {
+	$('#red').toggleClass("clickedRed");
+}
+
+function yellow() {
+	$('#yellow').toggleClass("clickedYellow");
+}
+
+function blue() {
+	$('#blue').toggleClass("clickedBlue");
+}
+
+$("#instructions").click(function() {
+	$('#box').show();
+});
+$("#close").click(function() {
+	$('#box').hide();
+})
