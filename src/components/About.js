5f1ffabf72243d9ea3ac2b473a976bac5662ed48
Sean Smith <sean.smith.2009@gmail.com>
2016-12-18 21:12:54 -0800

    updated portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ export default class About extends React.Component {
     		<div id="wrapper">
 	     	 <img className="avatar" src="/assets/images/avatar.png" alt="Personal Avatar" />
 	      	<h1 className="name">Sean Smith</h1>
-	      	<p className="nameAbout">I am a former scientist, a business owner, and a software developer by way of Free Code Camp. I love working with React, Redux, and Node and I am currently seeking employment in San Francisco.</p>
-	      	<p className="thanks">Thanks for visiting my site!</p>
+	      	<p className="nameAbout">I am a software engineer and Free Code Camp core contributor. I love working with React, Redux, JavaScript, and Node and I am currently seeking employment in San Francisco.</p>
+	      	<p className="thanks">See some of my projects below</p>
     		</div>
   		</div>
 		);
