52d45f4f9481477c1f95af26464b842639b92d9a
Sean Smith <sean.smith.2009@gmail.com>
2016-12-29 11:29:16 -0800

    cleaned up code organization

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/5_projects-mobile.css';
 import './styles/1_index.css';
-
-import './styles/images-CDN.css';
-//import './styles/2_project-images.css';
-
+import './styles/2_project-images.css';
 import './styles/3_image-base.css';
 import './styles/4_hover-styling.css';
 import './styles/6_mobile-styling.css';
