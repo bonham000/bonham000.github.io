defcdd88e18b4cc9d3ccce968f3958c6bb5bf979
Sean Smith <sean.smith.2009@gmail.com>
2017-01-04 01:21:30 -0800

    modified bio text and styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,10 @@ export default function About() {
   		<div id="wrapper">
      	 <img className="avatar" src="http://res.cloudinary.com/sean-smith-me/image/upload/q_25/v1483040881/gravatar-min_dkoddy.png" alt="Personal Avatar" />
       	<h1 className="name">Sean Smith</h1>
-      	<p className="nameAbout">I am a software engineer and Free Code Camp core contributor.
-      	My primary experience is with React, Redux, and Node and I am currently seeking employment in San Francisco.</p>
-      	<p className="thanks">See some of my projects below</p>
+      	<p className="nameAbout">I am a software engineer and open source contributor.</p>
+      	<p className="nameSub">Most recently I led the development of the React/Redux curriculum expansion at Free Code Camp.
+        I am currently looking for employment as a JavaScript developer in San Francisco.</p>
+      	<p className="thanks">See my work below:</p>
   		</div>
 		</div>
 	);
