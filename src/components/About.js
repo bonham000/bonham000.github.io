9a2688abab2a252c6c441d7149a956b872a809e6
Sean Smith <sean.smith.2009@gmail.com>
2017-03-02 22:59:24 -0500

    updated subtitle and content

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,7 @@ export default function About(props) {
   		<div id="wrapper">
      	 <img className="avatar" src="http://res.cloudinary.com/sean-smith-me/image/upload/q_25/v1483040881/gravatar-min_dkoddy.png" alt="Personal Avatar" />
       	<h1 className="name">Sean Smith</h1>
-      	<p className="nameAbout">Lifetime freeCodeCamper</p>
-      	<p className="nameSub">I led the development of the React/Redux curriculum expansion at Free Code Camp, an international open-source learning community which teaches students to code and builds pro bono software for non-profit organizations.</p>
+      	<p className="nameSub">I led the development of the React/Redux curriculum expansion at <a target="_blank" href="https://github.com/freeCodeCamp/freeCodeCamp" id="fccLink">freeCodeCamp</a>, an international open-source learning community which teaches students to code and builds pro bono software for non-profit organizations.</p>
       	<p className="arrow">
           <i
             id="projectsArrow"
