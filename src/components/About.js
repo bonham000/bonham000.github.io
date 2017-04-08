9718b1dc93b4b655cbe1d340fea994e3efbcca78
Sean Smith <sean.smith.2009@gmail.com>
2017-04-08 10:55:52 -0700

    Added new profile image

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function About(props) {
 	return (
-		 <div className="main" id='homeNav'> 
+		 <div className="main" id='homeNav'>
   		<div id="wrapper">
-     	 <img className="avatar" src="http://res.cloudinary.com/sean-smith-me/image/upload/q_25/v1483040881/gravatar-min_dkoddy.png" alt="Personal Avatar" />
+     	 <img className="avatar" src="https://res.cloudinary.com/sean-smith-me/image/upload/q_50/v1491674073/portfolio/profile-avatar_eeksgk.jpg" alt="Personal Avatar" />
       	<h1 className="name">Sean Smith</h1>
       	<p className="nameSub">I led the development of the React/Redux curriculum expansion at <a target="_blank" href="https://github.com/freeCodeCamp/freeCodeCamp" id="fccLink">freeCodeCamp</a>, an international open-source learning community which teaches students to code and builds pro bono software for non-profit organizations.</p>
       	<p className="arrow">
