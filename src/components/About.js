cf04e669a5fa2ddcf0b5a9a5cb5808e293b5b90c
Sean Smith <sean.smith.2009@gmail.com>
2017-05-13 20:16:36 -0700

    Revises and updates About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,45 @@
 import React from 'react'
 
+const TruSTAR = (
+	<a
+		className="aboutLink"
+		target="_blank"
+		href="https://www.trustar.co/">
+		TruSTAR Technology
+	</a>
+);
+
+const FCC = (
+	<a
+		className="aboutLink"
+		target="_blank"
+		href="https://github.com/freeCodeCamp/freeCodeCamp">
+		freeCodeCamp
+	</a>
+);
+
+const Medium = (
+	<a
+		className="aboutLink"
+		target="_blank"
+		href="https://medium.freecodecamp.com/how-i-learned-to-code-and-earned-a-job-in-silicon-valley-changing-my-life-along-the-way-a3af854855fa">
+		here
+	</a>
+);
+
+
 export default function About(props) {
 	return (
 		 <div className="main" id='homeNav'>
-  		<div id="wrapper">
+  		<div>
      	 <img className="avatar" src="https://res.cloudinary.com/sean-smith-me/image/upload/q_50/v1491674073/portfolio/profile-avatar_eeksgk.jpg" alt="Personal Avatar" />
       	<h1 className="name">Sean Smith</h1>
-      	<p className="nameSub">I led the development of the React/Redux curriculum expansion at <a target="_blank" href="https://github.com/freeCodeCamp/freeCodeCamp" id="fccLink">freeCodeCamp</a>, an international open-source learning community which teaches students to code and builds pro bono software for non-profit organizations.</p>
-      	<p className="arrow">
-          <i
-            id="projectsArrow"
-            className="fa fa-arrow-down fa-arrow"
-            aria-hidden="true"
-            onClick={props.navigate}>
-          </i>
-        </p>
+      	<p className="nameSub">
+					I am a software engineer at {TruSTAR} in San Francisco. I
+					learned to code at {FCC} and wrote about my journey {Medium}.
+					You can find all the projects I built during my education below.
+					Thanks for visiting!
+				</p>
   		</div>
 		</div>
 	);
